d3dbb992f583a1730547811d9baee42944d50d8e
maina michael <pantalemonmongah@gmail.com>
2024-01-19 00:12:36 +0300

    Fix: Betty style

--- monty.h
+++ monty.h
@@ -8,7 +8,8 @@
 #include <string.h>
 #include <ctype.h>
 /**
- * struct stack_struct - doubly linked list representation of a stack (or queue)
+ * struct stack_struct - doubly linked list representation
+ * of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
